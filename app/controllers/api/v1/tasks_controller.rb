45180fba1b0a207bf6121a53397cc648d6c6e8f7
Tsutsumi Tomoharu <ttomoharu201@gmail.com>
2020-06-20 14:36:21 +0900

    axios activate

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,8 +1,10 @@
 class Api::V1::TasksController < ApplicationController
+  skip_before_action :verify_authenticity_token
   #serializer?
   def index
   end
 
   def create
+    head :no_content
   end
 end
--- app/javascript/sagas/taskSaga.js
+++ app/javascript/sagas/taskSaga.js
@@ -1,7 +1,17 @@
-import { put, takeEvery, all } from 'redux-saga/effects'
+import { put, takeEvery, all, call } from 'redux-saga/effects'
+import axios from 'axios'
+
+function createTask(taskParams) {
+  return axios.request({
+    method: 'post',
+    url: '/api/v1/tasks',
+    data: taskParams
+  })
+}
 
 function* addTask(action){
   const task = action.payload.task
+  yield call(createTask, task)
   yield put({type: 'ADD_TASK', task: task})
 }
 
